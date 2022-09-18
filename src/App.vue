c6263b44da834facee07c677165262abab2e9ab8
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-18 07:00:13 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Calculator from './components/Calculator.vue'
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+</template>
+
+<style>
+  body {
+    background: #31475e !important;
+  }
+</style>
